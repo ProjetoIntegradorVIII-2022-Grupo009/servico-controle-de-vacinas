393a06d0293bfdcd4999b6cc4c0089b9d57dbaa2
Regina Nakagima <reginahisaegoncalves@gmail.com>
2022-11-15 09:34:42 -0300

    atualizações pacotes yarn

--- servico-controle-de-vacinas-frontend/src/components/pages/Catastrofes/index.tsx
+++ servico-controle-de-vacinas-frontend/src/components/pages/Catastrofes/index.tsx
@@ -16,10 +16,170 @@ function Catastrofes() {
                 <div>
                     <h1 className="titulo-catastrofes">Catástrofes</h1>
                     <br></br>
+                    <br></br>
                     <div>
                         <p>Catástrofes são desastres de grandes proporções relacionados a fenômenos naturais.</p>
                     </div>
                     <br></br>
+                    <h2>O que é desastre ambiental?</h2>
+                    <p>Desastres ambientais são eventos que afetam negativamente uma sociedade ou ecossistema em algum nível, podem ter origem natural, por exemplo: terremotos, tsunamis, furacões e outros eventos naturais, ou podem ocorrer por intervenções humanas, tais como: rompimento de barragens, acidentes em usinas nucleares, vazamento óleo no mar. </p>
+                    <h2>Desastres ambientais no Brasil</h2>
+                    <div>
+                        <p>
+                            No Brasil, os desastres ambientais dificilmente são provocados por efeitos naturais de grande magnitude, porém, muitos deles são provocados por ações humanas.
+                            Como os dois grandes desastres ambientais do Brasil: acidente Césio 137 em Goiânia em 1987 e o rompimento da barragem de Brumadinho em 2019.
+                        </p>
+                        <br></br>
+                        <div className="container row">
+                            <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                <div className="card">
+                                    <iframe src="https://www.youtube.com/embed/hP-8D-YglDo" title="Acidente Césio 137 Goiânia" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Acidente Césio 137 Goiânia</h5>
+                                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                <div className="card">
+                                    <iframe src="https://www.youtube.com/embed/YIN02W40UTE" title="Brumadinho: o documentário da BBC (PARTE 1)" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Brumadinho: o documentário da BBC (PARTE 1)</h5>
+                                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                <div className="card">
+                                    <iframe src="https://www.youtube.com/embed/TUlq8pjOU4U" title="Brumadinho: o documentário da BBC (PARTE 2)" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Brumadinho: o documentário da BBC (PARTE 2)</h5>
+                                        <p className="card-text">Continuação do documentário da BBC.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div>
+                        <h2>Os maiores desastres naturais do Brasil</h2>
+                        <p>Alguns acontecimentos mais destrutivos do Brasil foram:</p>
+                        <div className="dez-maiores-desastres">
+                            <p>
+                                <li> Incêndios:</li>
+                                <ul className="b">
+                                    <li>1963 - Incêndio no Paraná;</li>
+                                    <li>1974 - Incêndio no edifício Joelma em São Paulo;</li>
+                                    <li>1998 - Incêndio de Roraima;</li>
+                                    <li>2013 - Incendio na boate Kiss no Rio Grande do Sul;</li>
+                                    <li>2017 - Incêncio na Chapada dos Veadeiros em Goiás;</li>
+                                    <li>2018 - Incêncio no museu nacional em São Paulo;</li>
+                                </ul>
+                                <li>Tornados:</li>
+                                <ul className="b">
+                                    <li>1991 - Tornado na cidade de Itu (São Paulo);</li>
+                                    <li>2005 - Tornado na cidade de Indaiatuba;(São Paulo);</li>
+                                    <li>2015 - Dois tornados no oeste de Santa Catarina;</li>
+                                    <li>2018 - Tornado em 24 cidades gaúchas;</li>
+                                </ul>
+                            </p>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <div className="container row d-flex justify-content-start">
+                            <div className="col-12 col-md-6 mb-2">
+                                <div className="card">
+                                    <iframe src="https://www.youtube.com/embed/BPvKYhScq08" title="Os maiores desastres naturais do Brasil" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Os maiores desastres naturais do Brasil</h5>
+                                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div>
+                        <h2>Desastres Naturais: Castigos da natureza ou desequilíbrio urbano?</h2>
+                        <p>
+                            No Brasil, os desastres ambientais dificilmente são provocados por efeitos naturais de grande magnitude, porém, muitos deles são provocados por ações humanas.
+                            Como os dois grandes desastres ambientais do Brasil: acidente Césio 137 em Goiânia em 1987 e o rompimento da barragem de Brumadinho em 2019.
+                        </p>
+                        <br></br>
+                        <div className="container row d-flex justify-content-start">
+                            <div className="col-12 col-md-6 mb-2">
+                                <div className="card">
+                                    <iframe src="https://www.youtube.com/embed/VYy2c6h1KsE" title="Desastres Naturais: Castigos da natureza ou desequilíbrio urbano?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Desastres Naturais: Castigos da natureza ou desequilíbrio urbano?</h5>
+                                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br></br>
+                        <div>
+                            <h2>Desastres ambientais no Mundo</h2>
+                            <p>
+                                No Brasil, os desastres ambientais dificilmente são provocados por efeitos naturais de grande magnitude, porém, muitos deles são provocados por ações humanas.
+                                Como os dois grandes desastres ambientais do Brasil: acidente Césio 137 em Goiânia em 1987 e o rompimento da barragem de Brumadinho em 2019.
+                            </p>
+                            <br></br>
+                            <div className="container row">
+                                <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                    <div className="card">
+                                        <iframe src="https://www.youtube.com/embed/DiGqjYkRQ6o" title="Chernobyl: A História Completa" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Chernobyl: A História Completa</h5>
+                                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                    <div className="card">
+                                        <iframe src="https://www.youtube.com/embed/5mw1JJ0KdFc" title="Fukushima: terremoto, tsunami e acidente nuclear" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                        <div className="card-body">
+                                            <h5 className="card-title">Fukushima: terremoto, tsunami e acidente nuclear</h5>
+                                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-12 col-md-6 col-lg-4 mb-2">
+                                    <div className="card">
+                                        <iframe src="https://www.youtube.com/embed/ZFgHuXHRSKE" title="3ª Guerra Mundial ou 1ª Ciberguerra?" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                                        <div className="card-body">
+                                            <h5 className="card-title">3ª Guerra Mundial ou 1ª Ciberguerra?</h5>
+                                            <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br></br>
+                        <div>
+                            <h2>Os Dez Maiores Desastres Naturais</h2>
+                            <div>
+                                <p>
+                                    No Brasil, os desastres ambientais dificilmente são provocados por efeitos naturais de grande magnitude, porém, muitos deles são provocados por ações humanas.
+                                    Como os dois grandes desastres ambientais do Brasil: acidente Césio 137 em Goiânia em 1987 e o rompimento da barragem de Brumadinho em 2019.
+                                </p>
+                                <br></br>
+                                <div className="container row d-flex justify-content-start">
+                                    <div className="col-12 col-md-6 mb-2">
+                                        <div className="card">
+                                            <iframe className="video9" src="https://www.youtube.com/embed/MbqIro1tm3U" title="Os Dez Maiores Desastres Naturais" sandbox="allow-scripts allow-same-origin allow-forms"></iframe>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Desastres Naturais: Castigos da natureza ou desequilíbrio urbano?</h5>
+                                                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <br></br>
+                                <br></br>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- servico-controle-de-vacinas-frontend/src/components/pages/Epidemiologia/index.tsx
+++ servico-controle-de-vacinas-frontend/src/components/pages/Epidemiologia/index.tsx
@@ -16,6 +16,14 @@ function Epidemiologia() {
                 <div>
                     <h1 className="titulo-epidemiologia">Epidemiologia</h1>
                     <br></br>
+                    <div className="card">
+                        <iframe src="https://www.youtube.com/embed/0ZEABB1LRFE" title="Epidemiologia: Conceitos Fundamentais" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <div className="card-body">
+                            <h4 className="card-title">As maiores pandemias da história</h4>
+                            <p className="card-text">Nenhuma outra doença viral atingiu proporções de letalidade como o surto do vírus COVID-19.</p>
+                        </div>
+                    </div>
+                    <br></br>
                     <p>Existem diferentes nomes técnicos para classificar o status epidemiológico de uma doença, são eles:</p>
                     <div className="classificacao">
                         <p>
@@ -27,6 +35,59 @@ function Epidemiologia() {
                             </ul>
                         </p>
                     </div>
+                    <br></br>
+                    <div>
+                        <h2>Surto</h2>
+                        <p>
+                            Surto ou evento inusitado em saúde pública: situação em que há aumento acima do esperado na ocorrência de casos de evento ou doença em uma área ou entre um grupo específico de pessoas, em determinado período. Ressalta-se que, para doenças raras, um único caso pode representar um surto.
+                            <br />
+                            A ocorrência de surtos pode ser identificada de várias maneiras. Entre elas destacam-se:
+                            <li>Notificação por profissionais de saúde que percebem em sua rotina uma elevação do número de casos de determinada doença ou de sua gravidade.</li>
+                            <li>Informação procedente da comunidade e notificada às autoridades.</li>
+                            <li>Informação procedente da imprensa.</li>
+                            <li>Análise de rotina de dados de vigilância epidemiológica. Tais dados podem ser obtidos por diversas formas, incluindo a notificação compulsória de doenças, inquéritos ou busca ativa em uma investigação, assim como a detecção laboratorial.</li>
+                            <br />
+                            A detecção de surtos exige a adoção de medidas de controle oportunas para o seu controle e prevenção da ocorrência de novos casos.
+                            <br />
+                            A maior parte dos surtos é de etiologia infecciosa e transmissível e muitas vezes representam razões para a realização de investigação sistemática com vistas à identificação da fonte de infecção e adoção das medidas de controle e elaboração de recomendações adicionais.
+                            <br />
+                            Entre outras razões para investigar surtos, incluem-se a oportunidade de:
+                            <br />
+                            a) descrever novas doenças; <br />
+                            b) aprender mais sobre doenças conhecidas; <br />
+                            c) avaliar as estratégias de prevenção e controle existentes, por exemplo, vacinas; <br />
+                            d) ensinar (e aprender) epidemiologia; e e) responder a uma preocupação da população sobre o surto. <br />
+                            Quando se decide por investigar um surto de forma sistematizada e com base no método científico, a depender do surto, pelo menos três tipos de atividades devem ser considerados: <br />
+                            <li> A investigação epidemiológica.</li>
+                            <li> A investigação ambiental.</li>
+                            <li> A interação e comunicação com a população e com a imprensa e, em alguns casos, com órgão de controle social (sistema legal).</li>
+                        </p>
+                        <br></br>
+                        <h2>Endemia</h2>
+                        <p>A endemia é a presença contínua de uma enfermidade ou de um agente infeccioso em uma zona geográfica determinada. </p>
+                        <br></br>
+                        <h2>Epidemia</h2>
+                        <p>
+                            A epidemia é uma denominação utilizada em situações em que a doença envolve grande número de pessoas e atinge uma larga área geográfica.
+                            <br />
+                            Neste caso, a doença se faz presente em diversos locais ou comunidades, para além daquele em que foram inicialmente identificados. As epidemias podem ser em nível municipal, estadual e nacional.
+                        </p>
+                        <br></br>
+                        <h2>Pandemia</h2>
+                        <p>
+                            Uma pandemia é a disseminação mundial de uma doença. Ela pode surgir quando um agente infeccioso se espalha ao redor do mundo e a maior parte das pessoas não são imunes a ele.
+                            Em uma escala de gravidade, a pandemia é o pior dos cenários porque ela se estende a várias regiões do planeta. Mas isso não necessariamente significa que a situação é irreversível ou que o agente da doença tenha aumentado o seu poder de ameaça.
+                        </p>
+                        <br></br>
+                    </div>
+                    <div className="card">
+                        <iframe src="https://www.youtube.com/embed/V4gFVuc4n9s" title="As maiores pandemias da história" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <div className="card-body">
+                            <h4 className="card-title">As maiores pandemias da história</h4>
+                            <p className="card-text">Nenhuma outra doença viral atingiu proporções de letalidade como o surto do vírus COVID-19.</p>
+                        </div>
+                    </div>
+                    <br></br>
                     <br></br>
                 </div>
             </div>
