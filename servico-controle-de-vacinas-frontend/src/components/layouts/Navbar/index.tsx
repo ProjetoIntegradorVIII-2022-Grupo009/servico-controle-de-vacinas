824c1bc90fd44f1cc01cfdd39447c34bb60db196
Marcos <marcosrmachado14@hotmail.com>
2022-11-15 21:59:49 -0300

    Atualização Cadastro

--- servico-controle-de-vacinas-frontend/src/components/layouts/Navbar/index.tsx
+++ servico-controle-de-vacinas-frontend/src/components/layouts/Navbar/index.tsx
@@ -1,33 +1,42 @@
 import { Link } from 'react-router-dom'
+import painelCatastrofes from "../../../assets/img/Login.png"
 import './styles.css'
 
 function Navbar() {
     return (
         <nav className="navbar">
-            <ul className="list">
-                <li className='item'>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/cadastro'>Cadastro</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/vacinas'>Vacinas</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/campanhas'>Campanhas</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/doencasTransmissiveis'>Doenças Transmissíveis</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/epidemiologia'>Epidemiologia</Link>
-                </li>
-                <li className='item'>
-                    <Link to='/catastrofes'>Catástrofes</Link>
-                </li>
-            </ul>
-        </nav>
+        <ul className="list">
+          
+          <a href="login">
+              <li className='item'>
+              <img src={painelCatastrofes} alt="login"/>
+              </li>                           
+                           
+            <li className='item'>
+            <Link to='/cadastro'><h6>Cadastre-se</h6></Link>
+            </li>                                
+          </a>                           
+              
+            <li className='item'>
+                <Link to='/home'>Home</Link>
+            </li>
+            <li className='item'>
+                <Link to='/vacinas'>Vacinas</Link>
+            </li>
+            <li className='item'>
+                <Link to='/campanhas'>Campanhas</Link>
+            </li>
+            <li className='item'>
+                <Link to='/doencasTransmissiveis'>Doenças Transmissíveis</Link>
+            </li>
+            <li className='item'>
+                <Link to='/epidemiologia'>Epidemiologia</Link>
+            </li>
+            <li className='item'>
+                <Link to='/catastrofes'>Catástrofes</Link>
+            </li>
+        </ul>        
+    </nav> 
     )
 }
 
--- servico-controle-de-vacinas-frontend/src/components/pages/Cadastro/index.tsx
+++ servico-controle-de-vacinas-frontend/src/components/pages/Cadastro/index.tsx
@@ -1,6 +1,110 @@
 function Cadastro() {
 return (
-    <h1>Cadastro</h1>
+    
+    <form className="row g-3">
+    <div className="col-md-4">
+      <label htmlFor="validationServer01" className="form-label">Nome<br/></label>
+      <input type="text" className="form-control is-valid" id="validationServer01"
+      required></input>
+      <div className="valid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-4">
+      <label htmlFor="validationServer02" className="form-label">CPF<br/></label>
+      <input type="text" className="form-control is-valid" id="validationServer02"
+      aria-describedby="validationServer02Feedback" required></input>
+      <div id="validationServer02Feedback" className="valid-feedback">
+      </div>
+    </div>  
+    <br></br>
+    <div className="col-md-4">
+      <label htmlFor="validationServerUsername" className="form-label">Email<br/></label>
+      <div className="input-group has-validation">        
+        <input type="text" className="form-control is-invalid" id="validationServerUsername" 
+        aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required></input>
+        <div id="validationServerUsernameFeedback" className="invalid-feedback">
+        </div>
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-6">
+      <label htmlFor="validationServer03" className="form-label">Endereço<br/></label>
+      <input type="text" className="form-control is-invalid" id="validationServer03" 
+      aria-describedby="validationServer03Feedback" required></input>
+      <div id="validationServer03Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-3">
+      <label htmlFor="validationServer05" className="form-label">Número<br/></label>
+      <input type="text" className="form-control is-invalid" id="validationServer05" aria-describedby=
+      "validationServer05Feedback" required></input>
+      <div id="validationServer05Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-3">
+      <label htmlFor="validationServer05" className="form-label">CEP<br/></label>
+      <input type="text" className="form-control is-invalid" id="validationServer05" aria-describedby=
+      "validationServer05Feedback" required></input>
+      <div id="validationServer05Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-3">
+      <label htmlFor="validationServer04" className="form-label">Cidade<br/></label>
+      <select className="form-select is-invalid" id="validationServer04" aria-describedby=
+      "validationServer04Feedback" required>
+        <option selected disabled value="">Opção...</option>
+        <option>Cabreúva</option>
+        <option>Indaiatuba</option>
+        <option>Itu</option>
+        <option>Porto Feliz</option>
+        <option>Salto</option>
+        <option>Sorocaba</option>
+        </select>
+      <div id="validationServer04Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-6">
+      <label htmlFor="validationServer03" className="form-label">Complemento<br/></label>
+      <input type="text" className="form-control is-invalid" id="validationServer03" 
+      aria-describedby="validationServer03Feedback" required></input>
+      <div id="validationServer03Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-md-3">
+      <label htmlFor="validationServer04" className="form-label">Sexo<br/></label>
+      <select className="form-select is-invalid" id="validationServer04" aria-describedby=
+      "validationServer04Feedback" required>
+        <option selected disabled value="">Opção...</option>
+        <option>Masculino</option>
+        <option>Feminino</option>
+        
+        </select>
+      <div id="validationServer04Feedback" className="invalid-feedback">
+      </div>
+    </div>
+    <br></br>
+    <div className="col-12">
+      <div className="form-check">
+        <input className="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3"
+        aria-describedby="invalidCheck3Feedback" required></input>
+        <label className="form-check-label" htmlFor="invalidCheck3">
+        </label> Aceito os termos de uso
+        <div id="invalidCheck3Feedback" className="invalid-feedback"> 
+        </div>
+      </div>
+    </div>
+    <br></br>
+    <div className="col-12">
+      <button className="btn btn-primary" type="submit">Enviar</button>
+    </div>
+    <br></br>
+  </form>  
 )
 }
 
