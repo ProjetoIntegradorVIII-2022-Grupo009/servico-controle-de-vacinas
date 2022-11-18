2a533ad0f04057b475067c764e8c0206d6a8b32d
Regina Nakagima <reginahisaegoncalves@gmail.com>
2022-11-18 12:19:42 -0300

    [Backend] Date select

--- sevico-controle-de-vacinas-backend/src/main/java/com/univespsalto2022piviiigrupo009/controledevacinas/services/VacinaService.java
+++ sevico-controle-de-vacinas-backend/src/main/java/com/univespsalto2022piviiigrupo009/controledevacinas/services/VacinaService.java
@@ -1,8 +1,12 @@
 package com.univespsalto2022piviiigrupo009.controledevacinas.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.univespsalto2022piviiigrupo009.controledevacinas.entities.Vacina;
@@ -14,7 +18,13 @@ public class VacinaService {
 	@Autowired
 	private VacinaRepository repository;
 	
-	public List<Vacina> findVacina() {
-		return repository.findAll();
+	public Page<Vacina> findVacinas(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findVacinas(min, max, pageable);
 	}
 }
