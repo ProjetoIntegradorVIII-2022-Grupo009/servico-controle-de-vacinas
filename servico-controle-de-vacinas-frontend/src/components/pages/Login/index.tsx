d10568c1090058554078491ab3a11efe89c1058c
Regina Nakagima <reginahisaegoncalves@gmail.com>
2022-11-17 21:44:57 -0300

    Alterações Marcos

--- servico-controle-de-vacinas-frontend/src/components/pages/Login/index.tsx
+++ servico-controle-de-vacinas-frontend/src/components/pages/Login/index.tsx
@@ -3,32 +3,33 @@ import './styles.css'
 function Login() {
   return (
 <>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
+
 <div className='login'>
+<br></br>
+<br></br>
+<br></br>
 <form className="row g-3">
+<div className="col-md-4">
+      <label htmlFor="validationServer02" className="form-label">CPF<br/></label>
+      <input type="text" className="form-control is-valid" id="validationServer02"
+      aria-describedby="validationServer02Feedback" required></input>
+      <div id="validationServer02Feedback" className="valid-feedback">
+      </div>
+    </div> 
+    <p></p>
   <div className="col-auto">
-    <label htmlFor="staticEmail2" className="visually-hidden">Email</label>
-    <input type="text" readOnly className="form-control-plaintext" id="staticEmail2" 
-    value="email@example.com"></input>
-  </div>
-  <div className="col-auto">
-    <label htmlFor="inputPassword2" className="visually-hidden">Password</label>
+    <label htmlFor="inputPassword2" className="form-label">Senha</label>
     <input type="password" className="form-control" id="inputPassword2" 
     placeholder="Password"></input>
   </div>
+  <p></p>
   <div className="col-auto">
-    <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
+  <button type="submit" className="btn btn-primary mb-3">Confirm identity</button>
   </div>
 </form>
+<br></br>
+<br></br>
 </div>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
 </>
   )
 }
